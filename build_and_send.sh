8f797575a73d8f3e16a4b9c3717de1c59337b3a5
kuche1 <znamsiparolata4esno@abv.bg>
2024-03-27 00:11:36 +0200

    update

--- build_and_send.sh
+++ build_and_send.sh
@@ -2,7 +2,18 @@
 
 set -euo pipefail
 
-#bash TiConstructor/build_app.sh fkngarnd fkngarnd
-bash TiConstructor/build_prog.sh fkngarnd fkngarnd
+echo 'Compiling...'
 
-tilp --no-gui --silent fkngarnd/fkngarnd.8xp > /dev/null
+# bash TiConstructor/build_prog.sh fkngarnd fkngarnd
+bash TiConstructor/build_app.sh fkngarnd fkngarnd
+
+echo 'Transfering...'
+
+# tilp --no-gui --silent fkngarnd/fkngarnd.8xp > /dev/null
+tilp --no-gui --silent fkngarnd/fkngarnd.8xk > /dev/null
+
+# if tilp refuses to transfer this because of invalid signature
+# you need to download `rabbitsign` from your package manager and
+# replace `TiConstructor/other_files/rabbitsign`
+
+echo 'Success'
