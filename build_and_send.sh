22372af886053c9826a8857ae1fb78dd8806f3f0
kuche1 <znamsiparolata4esno@abv.bg>
2024-03-27 11:05:03 +0200

    update

--- build_and_send.sh
+++ build_and_send.sh
@@ -9,10 +9,14 @@ echo 'Compiling...'
 # bash TiConstructor/build_prog.sh fkngarnd fkngarnd
 bash "$HERE/build_files/build_app.sh" fkngarnd fkngarnd
 
+COMFILED_FILE="$HERE/fkngarnd/fkngarnd.8xk"
+
+echo "Compiled binary size: $(stat -c %s "$COMFILED_FILE") bytes"
+
 echo 'Transfering...'
 
 # tilp --no-gui --silent fkngarnd/fkngarnd.8xp > /dev/null
-tilp --no-gui --silent fkngarnd/fkngarnd.8xk > /dev/null
+tilp --no-gui --silent "$COMFILED_FILE" > /dev/null
 
 # if tilp refuses to transfer this because of invalid signature
 # you need to download `rabbitsign` from your package manager and
